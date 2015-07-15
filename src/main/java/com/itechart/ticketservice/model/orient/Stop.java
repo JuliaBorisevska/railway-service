2d8882a387e0ecb4317e99ddd495cf7f9a44e725
yulia.baryseuskaya <yulia.baryseuskaya@wsb-117.icx.local>
2015-07-15 06:59:10 -0300

    transform vertex to entity

--- src/main/java/com/itechart/ticketservice/model/orient/Stop.java
+++ src/main/java/com/itechart/ticketservice/model/orient/Stop.java
@@ -1,13 +1,40 @@
 package com.itechart.ticketservice.model.orient;
 
 import java.util.List;
+import java.util.Set;
+
+import com.tinkerpop.blueprints.Vertex;
 
 public class Stop {
 
+	private static final String ID_KEY = "id";
+	private static final String TITLE_KEY = "title";
+	private static final String TRAIN_IDS_KEY = "train_id";
+	
+	
+	private String uniqueLabel;
 	private Long id;
 	private String title;
-	private List<Long> trainIds;
+	private Set<Long> trainIds;
 	
+	public Stop() {
+	}
+	
+	public Stop(Vertex vertex) {
+		setUniqueLabel(vertex.getId().toString());
+		setId((Long)vertex.getProperty(ID_KEY));
+		setTitle(vertex.getProperty(TITLE_KEY).toString());
+		setTrainIds((Set<Long>)vertex.getProperty(TRAIN_IDS_KEY));
+	}
+	
+	public String getUniqueLabel() {
+		return uniqueLabel;
+	}
+
+	public void setUniqueLabel(String uniqueLabel) {
+		this.uniqueLabel = uniqueLabel;
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -20,11 +47,17 @@ public class Stop {
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public List<Long> getTrainIds() {
+	public Set<Long> getTrainIds() {
 		return trainIds;
 	}
-	public void setTrainIds(List<Long> trainIds) {
+	public void setTrainIds(Set<Long> trainIds) {
 		this.trainIds = trainIds;
+	}
+
+	@Override
+	public String toString() {
+		return "Stop [uniqueLabel=" + uniqueLabel + ", id=" + id + ", title="
+				+ title + ", trainIds=" + trainIds + "]";
 	}
 	
 	
--- src/main/java/com/itechart/ticketservice/repository/orient/StopRepositoryImpl.java
+++ src/main/java/com/itechart/ticketservice/repository/orient/StopRepositoryImpl.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.itechart.ticketservice.model.orient.Stop;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraph;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
@@ -14,6 +15,7 @@ import com.tinkerpop.gremlin.java.GremlinPipeline;
 import com.tinkerpop.pipes.PipeFunction;
 import com.tinkerpop.pipes.branch.LoopPipe;
 import com.tinkerpop.pipes.branch.LoopPipe.LoopBundle;
+import com.tinkerpop.pipes.transform.PathPipe;
 
 @Repository
 public class StopRepositoryImpl implements StopRepository{
@@ -35,6 +37,10 @@ public class StopRepositoryImpl implements StopRepository{
 	    	}
 	    	final Vertex v1 = graph.getVertex("#12:1");
 	    	final Vertex v2 = graph.getVertex("#12:10");
+	    	Stop firstStop = new Stop(v1);
+	    	Stop lastStop = new Stop(v2);
+	    	LOGGER.debug("First stop  - {}", firstStop);
+	    	LOGGER.debug("Last stop - {}", lastStop);
 	    	LOGGER.debug("Stop v1 title - {}",v1.getProperty("title"));
 	    	LOGGER.debug("Stop v2 title - {}",v2.getProperty("title"));
 	    	GremlinPipeline<Vertex,List> pipe = new GremlinPipeline<Vertex,List>().start(v1).as("x").out().
@@ -46,6 +52,7 @@ public class StopRepositoryImpl implements StopRepository{
                              }
                       }).path();
 	    	for (final List path : pipe) {
+	    		//PathPipe pathPipe = (PathPipe)path;
 	    		LOGGER.debug("Path: {}",path);
 	    	}
 	    } catch (Exception e) {                                               ///change!!
